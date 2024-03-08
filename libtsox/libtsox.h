1ca5631dab2c3e1a107f5f0f695b8d2ad259b665
toot_hzf <toot@tootzoe.com>
2024-03-09 01:30:03 +0700

    Initial Commit

---
+++ libtsox/libtsox.h
@@ -0,0 +1,24 @@
+//
+//  libtsox.h
+//  libtsox
+//
+//  Created by thor on 9/3/24
+//  
+//
+//  Email: toot@tootzoe.com  Tel: +855 69325538 
+//
+//
+
+
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for libtsox.
+FOUNDATION_EXPORT double libtsoxVersionNumber;
+
+//! Project version string for libtsox.
+FOUNDATION_EXPORT const unsigned char libtsoxVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <libtsox/PublicHeader.h>
+
+
